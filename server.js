4bcc3134eb9c965d6460cfa71121a3446cffb848
Rich <mayos53686@fw025.com>
2023-07-10 17:41:43 +1000

    file upload working

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const upload = multer({ dest: 'uploads/' });
 
 const app = express();
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
