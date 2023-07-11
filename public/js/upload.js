6eb911812cd1f02efc3431ffe44a2545a064c8b5
Rich <mayos53686@fw025.com>
2023-07-11 17:36:59 +1000

    getting dropzone working

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,34 +1,68 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const messagesDiv = document.getElementById("messages");
-    const message = messagesDiv.getAttribute("data-message");
-    const filename = messagesDiv.getAttribute("data-filename");
-  
-    if (message && filename) {
+  const messagesDiv = document.getElementById("messages");
+  const message = messagesDiv.getAttribute("data-message");
+  const filename = messagesDiv.getAttribute("data-filename");
+
+  if (message && filename) {
       messagesDiv.innerHTML = message + ' <a href="/uploads/' + filename + '">Download text file</a>';
-    }
-  
-    const uploadForm = document.getElementById("upload-form");
-    const inputFile = document.getElementById("file");
-  
-    uploadForm.addEventListener("submit", (event) => {
+  }
+
+  const uploadForm = document.getElementById("upload-form");
+  const inputFile = document.getElementById("file");
+  const dropZone = document.getElementById("drop-zone");
+
+  uploadForm.addEventListener("submit", (event) => {
       event.preventDefault();
-  
+
       const file = inputFile.files[0];
       const formData = new FormData();
       formData.append("file", file);
-  
+
       const xhr = new XMLHttpRequest();
       xhr.open("POST", "/upload");
       xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          const response = JSON.parse(xhr.responseText);
-          const convertedMessage = response.message;
-          const convertedFilename = response.filename;
-  
-          messagesDiv.innerHTML = convertedMessage + ' <a href="/uploads/' + convertedFilename + '">Download text file</a>';
-        }
+          if (xhr.readyState === 4 && xhr.status === 200) {
+              const response = JSON.parse(xhr.responseText);
+              const convertedMessage = response.message;
+              const convertedFilename = response.filename;
+
+              messagesDiv.innerHTML = convertedMessage + ' <a href="/uploads/' + convertedFilename + '">Download text file</a>';
+          }
       };
       xhr.send(formData);
-    });
   });
-  
+
+  ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+      dropZone.addEventListener(eventName, preventDefaults, false);
+  });
+
+  ['dragenter', 'dragover'].forEach(eventName => {
+      dropZone.addEventListener(eventName, highlight, false);
+  });
+
+  ['dragleave', 'drop'].forEach(eventName => {
+      dropZone.addEventListener(eventName, unhighlight, false);
+  });
+
+  dropZone.addEventListener('drop', handleDrop, false);
+
+  function preventDefaults(e) {
+      e.preventDefault();
+      e.stopPropagation();
+  }
+
+  function highlight(e) {
+      dropZone.classList.add('highlight');
+  }
+
+  function unhighlight(e) {
+      dropZone.classList.remove('highlight');
+  }
+
+  function handleDrop(e) {
+      const dt = e.dataTransfer;
+      const files = dt.files;
+
+      inputFile.files = files;
+  }
+});
